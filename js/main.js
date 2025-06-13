c743d2e58647ed8c3b018624e95a46f6ee01d72a
Suren <khe4oyan@gmail.com>
2025-06-13 14:07:42 +0400

    .

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import Constructor from "../classes/Constructor.js";
 
 const constructor = new Constructor(100, 100);
 
-console.log(constructor.newCube(0, 0, 0));
+constructor.newCube(0, 0, 0);
 constructor.newCube(1, 1, 0);
 constructor.newCube(0, 1, 0);
